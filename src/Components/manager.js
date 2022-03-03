68cea8a41f1f37a23a300bf93fe44c5ef38c0177
TamarRubin <tamarubin8484@gmail.com>
2022-03-03 22:26:36 +0200

    manager

--- src/Components/manager.js
+++ src/Components/manager.js
@@ -1,9 +1,99 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Container, Grid } from '@mui/material'
+import Ad from './Ad'
+import { Button } from 'react-bootstrap'
 
 const Manager = () => {
-    return (
-     <h1>manager   !!!</h1>
-    );
+  const [adsID, setAdsID] = useState([])
+  const [users, setusers] = useState([])
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/getAllUsers')
+      .then(res => {
+        console.log(res.data)
+        setusers(res.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  useEffect(() => {
+    let unmounted = false
+    axios.get('http://localhost:5000/getAllAdsToConfirm').then(res => {
+      
+            // for (var i = 0; i < res.data.length; i++) {
+             
+            //   setAdsID(state =>[...state, res.data[i]])
+            //   console.log(res.data[i].id);
+            // }
+      console.log('adsID', res.data)
+
+      for (var i = 0; i < res.data.length; i++) {
+        debugger
+        setAdsID(state => [...state, res.data[i]])
+        console.log(res.data[i].id)
+        console.log('adsID', adsID)
+      }
+    })
+    return () => {
+      unmounted = true
+    }
+  }, [])
+  const setStatus = id => {
+    axios.post('http://localhost:5000/ConfirmAdById/' + id).then(res => {
+      setAdsID(adsID.filter(a => a.id != id))
+      console.log('ConfirmAdById', res.data)
+    })
+  }
+  return (
+    <div>
+      {/* <h1> HELLO manager !!! </h1> */}
+
+      <Container sx={{ py: 8 }} maxWidth='md'>
+        {/* End hero unit */}
+        <Grid container spacing={15}>
+          {adsID.map(card => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+              <div>
+                <span>
+                  <Button
+                    variant='primary'
+                    onClick={() => {
+                      setStatus(card.id)
+                    }}
+                  >
+                    מאושר
+                  </Button>
+                </span>
+                <span>
+                  <Button variant='primary' onClick={() => {}}>
+                   לא מאושר
+                  </Button>
+                </span>
+              </div>
+              <Ad ad={card}></Ad>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+     
+      {/* {adsID[0]?.id}
+      {adsID.map(ad => (
+        <div>
+          <p>{ad.bookID}</p>
+          <p>{ad.id}</p>
+          <p>{ad.price}</p>
+          <p>{ad.status}</p>
+          <p>{ad.userID}</p>
+        </div> */}
+        {/* // bookID: 3
+        // id: 3
+        // price: 25
+        // status: 0
+        // userID: 3 */}
+      {/* ))} */}
+    </div>
+  )
 }
 
-export default Manager;
+export default Manager
